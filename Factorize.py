b126899e8cf98cb33029b8822651730b7e402782
SergiiOshmarin <113200315+SergiiOshmarin@users.noreply.github.com>
2022-12-18 20:46:30 +0100

    commit1

--- Factorize.py
+++ Factorize.py
@@ -23,8 +23,8 @@ def factorize(*numbers):
     return factors_list  
 
 '''Test the function with a list of numbers'''
-a, b, c, d  = factorize(128, 255, 99999, 10651060)
-print(f"assert a == {a}")
-print(f"assert b == {b}")
-print(f"assert c == {c}")
-print(f"assert d == {d}")
+a, b, c, d = factorize(128, 255, 99999, 10651060)
+assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
